3bba91c43ac62591486177660e301e97d6ec5454
Bence Jurás <strongerh@gmail.com>
2018-05-11 17:42:17 +0200

    no more error for json

--- Gomoku/src/main/java/pkg/Files/FileFunctions.java
+++ Gomoku/src/main/java/pkg/Files/FileFunctions.java
@@ -1,4 +1,5 @@
 package pkg.Files;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
@@ -32,7 +33,7 @@ import pkg.util.ParsedLine;
  * Contains every file related operation.
  */
 public class FileFunctions {
-	
+
 	/**
 	 * For reading from file.
 	 */
@@ -49,7 +50,7 @@ public class FileFunctions {
 	 * Parsed information from the file.
 	 */
 	private ParsedLine pl;
-	
+
 	/**
 	 * About info.
 	 */
@@ -57,244 +58,241 @@ public class FileFunctions {
 	/**
 	 * Position.
 	 */
-	private int x,y;
+	private int x, y;
 	/**
 	 * X or O.
 	 */
 	private char xOrO;
-	
+
 	/**
-	 * Creates the {@link FileFunctions} object.
-	 * Initialises the name of the {@link about} file.
+	 * Creates the {@link FileFunctions} object. Initialises the name of the
+	 * {@link about} file.
 	 */
-	public FileFunctions()
-	{
-		about="about";
+	public FileFunctions() {
+		about = "about";
 	}
 
 	/**
 	 * From the line, it takes out the x,y position, and the X/O character.
-	 * @param line  to parse
+	 * 
+	 * @param line
+	 *            to parse
 	 */
-	public void parseLine(String line)
-	{
-		String [] arr = line.split(";");
-		x= Integer.parseInt(arr[0]);
-		y= Integer.parseInt(arr[1]);
-		xOrO= arr[2].charAt(0);	
+	public void parseLine(String line) {
+		String[] arr = line.split(";");
+		x = Integer.parseInt(arr[0]);
+		y = Integer.parseInt(arr[1]);
+		xOrO = arr[2].charAt(0);
 	}
-	
+
 	/**
 	 * Creates a file with the given filename from the board then saves it.
 	 * 
-	 * @param filename for saving
-	 * @param board from saving
+	 * @param filename
+	 *            for saving
+	 * @param board
+	 *            from saving
 	 */
-	public void saveFile(String filename,char[][]board)
-	{
-			
-			Path path = FileSystems.getDefault().getPath("matches/", filename);
-			
-			if(!Files.exists(path))
-			{
-				File file = new File("matches");
-				file.mkdir();
-			}
-			
-			Charset charset = Charset.forName("US-ASCII");
-			
-			try (BufferedWriter writer = Files.newBufferedWriter(path, charset)) {
-				
-				for(int x=0;x<board.length;x++)
-				{
-					for(int y=0;y<board[0].length;y++)
-					{
-						if(board[x][y]!= '.' )
-						{
-							String line=x+";"+y+";"+board[x][y]+"\n";
-						    writer.write(line, 0, line.length());
-						}
+	public void saveFile(String filename, char[][] board) {
+
+		Path path = FileSystems.getDefault().getPath("matches/", filename);
+
+		if (!Files.exists(path)) {
+			File file = new File("matches");
+			file.mkdir();
+		}
+
+		Charset charset = Charset.forName("US-ASCII");
+
+		try (BufferedWriter writer = Files.newBufferedWriter(path, charset)) {
+
+			for (int x = 0; x < board.length; x++) {
+				for (int y = 0; y < board[0].length; y++) {
+					if (board[x][y] != '.') {
+						String line = x + ";" + y + ";" + board[x][y] + "\n";
+						writer.write(line, 0, line.length());
 					}
 				}
-			} catch (IOException x) {
-			    Logging.getLogger().error(x.getMessage());
 			}
-	      Logging.getLogger().info("File saved by the name: {}", filename);
+		} catch (IOException x) {
+			Logging.getLogger().error(x.getMessage());
+		}
+		Logging.getLogger().info("File saved by the name: {}", filename);
 	}
-	
+
 	/**
 	 * Reads in the about file.
 	 * 
 	 * @return {@code List<String>} object that contains the about file's lines.
 	 */
-	public  List<String> AboutString()
-	{
-		List<String> lines= new ArrayList<String>();
-		
+	public List<String> AboutString() {
+		List<String> lines = new ArrayList<String>();
+
 		Path path = FileSystems.getDefault().getPath("other_stuff/", about);
 		Charset charset = Charset.forName("US-ASCII");
-		
-		StringBuilder sb= new StringBuilder();
-		
+
+		StringBuilder sb = new StringBuilder();
+
 		try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
-		    String line = null;
-		    while ((line = reader.readLine()) != null) {
-		        lines.add(line);
-		    }
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				lines.add(line);
+			}
 		} catch (IOException x) {
-		    Logging.getLogger().error(x.getMessage());
+			Logging.getLogger().error(x.getMessage());
 		}
-		
+
 		Logging.getLogger().info("About text stored in List<String>");
 		return lines;
 	}
-	
+
 	/**
 	 * Loads the file, and builds a list of {@link ParsedLine} objects.
 	 * 
-	 * @param filename the name of the file, that it reads.
+	 * @param filename
+	 *            the name of the file, that it reads.
 	 * @return list of {@link ParsedLine} objects.
 	 */
-	public List<ParsedLine> loadFile(String filename)
-	{
-		List<ParsedLine> plines= new ArrayList<ParsedLine>();
+	public List<ParsedLine> loadFile(String filename) {
+		List<ParsedLine> plines = new ArrayList<ParsedLine>();
 		Charset charset = Charset.forName("US-ASCII");
 		Path path = FileSystems.getDefault().getPath("matches/", filename);
-		
+
 		try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
-		    String line = null;
-		    while ((line = reader.readLine()) != null) {
-		    	parseLine( line);
-		        plines.add(new ParsedLine(x,y,xOrO));
-		    }
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				parseLine(line);
+				plines.add(new ParsedLine(x, y, xOrO));
+			}
 		} catch (IOException x) {
-		    Logging.getLogger().error(x.getMessage());
+			Logging.getLogger().error(x.getMessage());
 		}
-		
+
 		Logging.getLogger().info("Game state lines stored in List<ParsedLine>");
 		return plines;
 	}
-	
+
 	/**
 	 * Loads the users in from the JSON slash Users dot json file.
+	 * 
 	 * @return {@code List<User>}
 	 */
-	public List<User> JSONloadUsers()
-	{
-		
+	public List<User> JSONloadUsers() {
+
 		Gson gson = new Gson();
-		Type listType = new TypeToken<ArrayList<User>>(){}.getType();
-		
+		Type listType = new TypeToken<ArrayList<User>>() {
+		}.getType();
+
 		List<User> users = new ArrayList<User>();
-		
-		try {
-			users = gson.fromJson(new FileReader("JSON/Users.json"), listType);
-			
-		} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
-			
-			Logging.getLogger().error("Error on parsing the Users.json file into List<User>. Error message: {}"
-					,e.getMessage());
+
+		if (new File("JSON/Users.json").isFile()) {
+			try {
+				users = gson.fromJson(new FileReader("JSON/Users.json"), listType);
+				Logging.getLogger().info("Loaded {} users.", users.size());
+
+			} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+
+				Logging.getLogger().error("Error on parsing the Users.json file into List<User>. Error message: {}",
+						e.getMessage());
+			}
+
 		}
-		
-		Logging.getLogger().info("Loaded {} users.",users.size());
-		
+
+		if (users == null) {
+			users = new ArrayList<User>();
+		}
+
 		return users;
 	}
-	
+
 	/**
 	 * Loads the matches in from the JSON slash Matches dot json file.
+	 * 
 	 * @return {@code List<Match>}
 	 */
-	public List<Match> JSONloadMatches()
-	{
-		GsonBuilder builder = new GsonBuilder(); 
+	public List<Match> JSONloadMatches() {
+		GsonBuilder builder = new GsonBuilder();
 		Gson gson = builder.create();
-		
-		Type listType = new TypeToken<ArrayList<Match>>(){}.getType();
-		
+
+		Type listType = new TypeToken<ArrayList<Match>>() {
+		}.getType();
+
 		List<Match> matches = new ArrayList<Match>();
-		
-		try {
-			matches = gson.fromJson(new FileReader("JSON/Matches.json"), listType);
-			
-			if(matches == null)
-			{
-				matches = new ArrayList<Match>();
+
+		if (new File("JSON/Matches.json").isFile()) {
+			try {
+				matches = gson.fromJson(new FileReader("JSON/Matches.json"), listType);
+				Logging.getLogger().info("Loaded {} matches.", matches.size());
+
+			} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+
+				Logging.getLogger().error("Error on parsing the Matches.json file into List<Match>. Error message: {}",
+						e.getCause());
 			}
-			
-		} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
-			
-			Logging.getLogger().error("Error on parsing the Matches.json file into List<Match>. Error message: {}"
-					,e.getCause());
 		}
-		
-		Logging.getLogger().info("Loaded {} matches.",matches.size());
-		
+
+		if (matches == null) {
+			matches = new ArrayList<Match>();
+		}
+
 		return matches;
 	}
-	
-	
+
 	/**
 	 * Saves a given user to the Users dot json file.
 	 * 
-	 * @param user to save.
+	 * @param user
+	 *            to save.
 	 */
-	public void JSONsaveUser(User user) 
-	{
+	public void JSONsaveUser(User user) {
 		Path path = FileSystems.getDefault().getPath("JSON/", "Users.json");
-		
-		if(!Files.exists(path))
-		{
+
+		if (!Files.exists(path)) {
 			File file = new File("JSON");
 			file.mkdir();
 		}
-		
+
 		List<User> users = JSONloadUsers();
 		users.add(user);
-		
-		users=users.stream().sorted((x,y)-> ((Integer)x.getUser_id()).compareTo(y.getUser_id()) ).
-		collect(Collectors.toList());
-		
-		try(Writer writer= new FileWriter("JSON/Users.json"))
-		{
+
+		users = users.stream().sorted((x, y) -> ((Integer) x.getUser_id()).compareTo(y.getUser_id()))
+				.collect(Collectors.toList());
+
+		try (Writer writer = new FileWriter("JSON/Users.json")) {
 			Gson gson = new GsonBuilder().create();
-			gson.toJson(users,writer);
+			gson.toJson(users, writer);
 		} catch (IOException e) {
-			Logging.getLogger().error("Erron with saving to Users.json. Error message: {}",e.getMessage());
+			Logging.getLogger().error("Erron with saving to Users.json. Error message: {}", e.getMessage());
 		}
 	}
-	
-	
+
 	/**
 	 * Saves a given match to the Users dot json file.
-	 * @param match to save
+	 * 
+	 * @param match
+	 *            to save
 	 */
-	public void JSONsaveMatch(Match match ) 
-	{
+	public void JSONsaveMatch(Match match) {
 		Path path = FileSystems.getDefault().getPath("JSON/", "Matches.json");
-		
-		if(!Files.exists(path))
-		{
+
+		if (!Files.exists(path)) {
 			File file = new File("JSON");
 			file.mkdir();
 		}
-		
+
 		List<Match> matches = JSONloadMatches();
 		matches.add(match);
-		
-		matches=matches.stream().sorted((x,y)-> ((Integer)x.getMatch_id()).compareTo(y.getMatch_id()) ).
-		collect(Collectors.toList());
-		
-		try(Writer writer= new FileWriter("JSON/Matches.json"))
-		{
-			Gson gson = new GsonBuilder()
-			        .create();
-			gson.toJson(matches,writer);
-			
+
+		matches = matches.stream().sorted((x, y) -> ((Integer) x.getMatch_id()).compareTo(y.getMatch_id()))
+				.collect(Collectors.toList());
+
+		try (Writer writer = new FileWriter("JSON/Matches.json")) {
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(matches, writer);
+
 		} catch (IOException e) {
-			Logging.getLogger().error("Erron with saving to Matches.json. Error message: {}",e.getMessage());
+			Logging.getLogger().error("Erron with saving to Matches.json. Error message: {}", e.getMessage());
 		}
 	}
-	
 
 }
